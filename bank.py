63b6814f022aa9d8bc20f0bd25494dba9b71a91f
Srikopiya Sivakumar <UT010115@unicomtic.com>
2025-05-10 14:30:40 +0530

    new update

--- bank.py
+++ bank.py
@@ -2,56 +2,57 @@ import os
 
 accounts={}
 next_account_number=10000
-next_customer_id=1
+next_customer_id=1 
 
-
+#create empty file
 def is_file_empty():
     try:
-        file=open("r")
-        data=file.read().strip()
-        file.close()
-        return data ==""
+        with open("admin.txt","r")as file:
+            data=file.read().strip()
+            return data == ""
     except:
         return True
 
 is_file_empty()
 
+#sign up admin
 def admin_signup():
+    if os.path.exists("admin.txt"):
+        print("Admin already registered.proceeding to login...\n")
+        return
     print("\n---Admin Sign-Up---")
     username=input("Create admin username:")
     password=input("Create admin password:")
-    file=open("admin.txt","w")
-    file.write(username +"\n"+ password)
-    file.close()
+    with open("admin.txt","w")as file:
+        file.write(username +"\n"+ password)
     print("Admin registered successfully.\n")
 
 admin_signup()
 
-def admin_login():
-    print("\n---Admin login---")
-    try:
-        file=open("admin.txt","r")
-        lines=file.readlines()
-        file.close()
-        if len(lines)< 2:
-            print("Admin credetials not found.")
-            return False
-        saved_user=lines[0].strip()
-        saved_pass=lines[1].strip()
-        username=input("enter admin username:")
-        password=input("enter admin password:")
-        if username==saved_user and password==saved_pass:
-            print("login successful!\n")
-            return True
-        else:
-            print("invalid credentials.")
-            return False
-    except:
+#login admin
+def admin_login(): 
+    while True:
+        print("\n---Admin login---")
+        try:
+            with open("admin.txt","r")as file:
+                lines=file.readlines()
+            if len(lines)< 2:
+                print("Admin credetials not found.")
+                return False
+            saved_user=lines[0].strip()
+            saved_pass=lines[1].strip()
+            username=input("enter admin username:")
+            password=input("enter admin password:")
+            if username==saved_user and password==saved_pass:
+                print("login successful!\n")
+                return True
+            else:
+                print("invalid credentials.")
+        except FileNotFoundError:
             print("Error reading admin file.")
-            return False
-    
-admin_login()
-
+            return False 
+        
+#valid number
 def is_valid_number(text):
     dot=0
     for c in text:
@@ -63,18 +64,21 @@ def is_valid_number(text):
             return False
     return len(text) > 0
 
+#creat account number
 def generate_account_number():
     global next_account_number
     accounts=str(next_account_number)
     next_account_number +=1
     return accounts
 
+#create customer ID
 def generate_customer_id():
     global next_customer_id
     cus_id="CUSTOMER"+ str(next_customer_id)
     next_customer_id+=1
     return cus_id
 
+#account creating
 def create_account():
     name=input("enter account holder name:")
     address=input("enter address:")
@@ -96,7 +100,7 @@ def create_account():
         'username':username,
         'password':password,
         'balance':float(balance),
-        'transaction':[]
+        'transactions':[]
     }
     print("Account created successfully.")
     print("Account number:",account_no)
@@ -106,6 +110,7 @@ def create_account():
 
     append_account_to_file(account_no,accounts[account_no])
 
+#save separate informations
 def save_user_credentials(cus_id,username,password):
     try:
         with open("user_credentials.txt","a") as file:
@@ -113,6 +118,7 @@ def save_user_credentials(cus_id,username,password):
     except:
         print("error writing user credentials to file")
 
+#amount deposit
 def deposit():
     account=input("Enter Account Number:")
     if account not in accounts:
@@ -124,11 +130,12 @@ def deposit():
         return
     amounts=float(amount)
     accounts[account]['balance']+=amounts
-    if "transaction" not in accounts[account]:
-        accounts[account]["transaction"]=[]
-    accounts[account]['transaction'].append(f"deposited{amounts}")
+    accounts[account]['transactions'].append(f"deposited{amounts}")
     print("deposit successfully.")
 
+    append_account_to_file(account,accounts[account])
+
+#amount withdraw
 def withdraw():
     account=input("enter account number:")
     if account not in accounts:
@@ -143,9 +150,12 @@ def withdraw():
         print("insufficient balance.")
         return
     accounts[account]['balance']-=amounts
-    accounts[account]['transaction'].append(f"withdrew{amount}")
+    accounts[account]['transactions'].append(f"withdrew{amount}")
     print("withdrawal successfully.")
 
+    append_account_to_file(account,accounts[account])
+
+#balance checking
 def check_balance():
     account=input("enter account number:")
     if account in accounts:
@@ -153,16 +163,17 @@ def check_balance():
     else:
         print("account not found.")
 
+#transaction history
 def transaction_history():
     account=input("enter account number:")
     if account in accounts:
         print("transaction history")
-        for t in accounts[account]['transaction']:
+        for t in accounts[account]['transactions']:
             print("-",t)
     else:
         print("account not fount")
 
-
+#customer information
 def customer_info():
     account=input("enter account number:")
     if account in accounts:
@@ -172,11 +183,13 @@ def customer_info():
         print("customer ID:",info['customer_id'])
         print("name:",info['name'])
         print("balance:",info['balance'])
-        print("transaction:")
-        for t in info['transaction']:
+        print("transaction:",[])
+        for t in info['transactions']:
             print("-",t)
     else:
         print("account not found.")
+
+#writing bank data file
 def append_account_to_file(account_no,info):
     try:
         file=open("bank_data.txt","a")
@@ -185,18 +198,19 @@ def append_account_to_file(account_no,info):
         file.close()
     except:
         print("successfully writing file.")
-
-
+    
+#save data in bank data text
 def save_all_data():
     try:
-        file=open("bank_data.txt","w")
-        for account_no,info in accounts.items():
-             line=account_no +"|"+info['customer_id']+"|"+info['name']+"|"+str(info['balance'])+"|"+",".join(info['transaction'])+"\n"
-        file.write(line)
-        file.close()
-    except:
-        print("error saving data.")
+        with open("bank_data.txt","w")as file:
+            for account_no,info in accounts.items():
+                line=account_no +"|"+info['customer_id']+"|"+info['name']+"|"+str(info['balance'])+"|"+",".join(info['transactions'])+"\n"
+                file.write(line)
+        print("All data saved successfully.")
+    except Exception as e:
+        print(f"error saving data.{e}")
 
+#load data in bank data text
 def load_data():
     global next_account_number,next_customer_id
     try:
@@ -224,6 +238,7 @@ def load_data():
 
 load_data()
 
+#main menu system
 def menu():
     while True:
         print("\n---Mini Banking---")
@@ -253,7 +268,9 @@ def menu():
             break
         else:
             print("Invalid Choice.")
-menu()
+
+if admin_login():
+    menu()
 
 
 
